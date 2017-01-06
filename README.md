9f399f04cf60ee4420a6aecec0bce33b085fc197
Halasi Tamás <trusted.tomato@gmail.com>
2017-01-06 18:46:08 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # IndexedDB storing preferences
 Simplified preference storing
+
+## How to use?
+Firstly, request opening the database with plain IndexedDB.
+
+	const dbOpenRequest = indexedDB.open('test-preference-storing');```
+
+Then send that to storePreferences.
+It returns a Promise, which resolves when the database is opened.
+The resolve function has one parameter,
+what can be used to set & get preferences.
+
+	storePreferences(dbOpenRequest).then(preferences => {/* set & get preferences */});
+
+- Setting preference: ```preferences.set('preferenceName','preferenceValue');```
+- Getting preference: ```preferences.get('preferenceName');``` -
+returns a Promise what resolves when it successfully gets the preference.
+The value of the preference is in the first parameter.
+If the preference exist, it's value is ```undefined```. 
+
+An example can be found in example.html.
+
+**Warning: the library uses the ```prefrences``` object store in the requested database.** 
--- index.js
+++ index.js
@@ -1,51 +1,37 @@
-window.storePreferences = (function(){
-	'use strict';
-
-	const storePreferences = indexedDBRequest => new Promise((resolve,reject) => {
-
-		indexedDBRequest.addEventListener('upgradeneeded', e => {
-			const db = e.target.result;
-			const dataObjectStore = db.createObjectStore('preferences',{keyPath: 'name'});
-			dataObjectStore.createIndex('name','name',{unique: true});
-			log('Database upgraded!');
-		});
-
-		indexedDBRequest.addEventListener('success', e => {
-			const db = e.target.result;
-			
-			resolve({
-				get: (dataName) =>
-					new Promise((resolve) => {
-						const transaction = db.transaction(['preferences'],'readonly');
-						const store = transaction.objectStore('preferences');
-						const index = store.index('name');
-						const gettingData = index.get(dataName);
-						gettingData.onsuccess = function(e){
-							const result = e.target.result;
-							resolve(result ? result.value : undefined);
-						};
-					}),
-				set: (dataName,dataValue) =>
-					new Promise((resolve) => {
-						const transaction = db.transaction(['preferences'],'readwrite');
-						const store = transaction.objectStore('preferences');
-						const puttingData = store.put({
-							name: dataName,
-							value: dataValue
-						});
-						puttingData.onsuccess = function(){
-							resolve();
-						};
-					})
-			});
-		});
+window.storePreferences = indexedDBRequest => new Promise((resolve,reject) => {
+	indexedDBRequest.addEventListener('upgradeneeded', e => {
+		const db = e.target.result;
+		const dataObjectStore = db.createObjectStore('preferences',{keyPath: 'name'});
+		dataObjectStore.createIndex('name','name',{unique: true});
 	});
 
-	const log = text => {
-		if(storePreferences.debug) console.log(text); 
-	};
-
-	storePreferences.debug = false;
-
-	return storePreferences;
-}());
+	indexedDBRequest.addEventListener('success', e => {
+		const db = e.target.result;
+		
+		resolve({
+			get: (dataName) =>
+				new Promise((resolve) => {
+					const transaction = db.transaction(['preferences'],'readonly');
+					const store = transaction.objectStore('preferences');
+					const index = store.index('name');
+					const gettingData = index.get(dataName);
+					gettingData.onsuccess = function(e){
+						const result = e.target.result;
+						resolve(result ? result.value : undefined);
+					};
+				}),
+			set: (dataName,dataValue) =>
+				new Promise((resolve) => {
+					const transaction = db.transaction(['preferences'],'readwrite');
+					const store = transaction.objectStore('preferences');
+					const puttingData = store.put({
+						name: dataName,
+						value: dataValue
+					});
+					puttingData.onsuccess = function(){
+						resolve();
+					};
+				})
+		});
+	});
+});
